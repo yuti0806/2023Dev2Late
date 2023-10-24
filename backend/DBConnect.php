17c772092abe6e6fda58d7600ad4930b9bd1b2c6
2101171 <114889924+2101171@users.noreply.github.com>
2023-10-24 12:09:33 +0900

    Delete backend directory

--- backend/DBConnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class Dbconnect{
-        public function dbConnect(){
-            // $pdo = new PDO('mysql:host=218.phy.lolipop.lan;dbname=LAA1418747-devlate;charset=utf8','root','DevLate');
-            $pdo = new PDO('mysql:host=localhost;dbname=LAA1418747-devlate;charset=utf8','root','root');
-            return $pdo;
-        }
-    }
-?>
