cd8e2813bc227153db05e63d46fa7ee6a7ffc7f6
2101171 <114889924+2101171@users.noreply.github.com>
2023-10-24 12:12:42 +0900

    Update DBConnect.php

--- 04_ソースコード/backend/DBConnect.php
+++ 04_ソースコード/backend/DBConnect.php
@@ -2,7 +2,7 @@
     class Dbconnect{
         public function dbConnect(){
             $pdo = new PDO('mysql:host=218.phy.lolipop.lan;dbname=LAA1418747-devlate;charset=utf8','root','DevLate');
-            // $pdo = new PDO('mysql:host=localhost;dbname=LAA1418747-devlate;charset=utf8','root','root');
+            // テスト用 $pdo = new PDO('mysql:host=localhost;dbname=LAA1418747-devlate;charset=utf8','root','root');
             return $pdo;
         }
     }
