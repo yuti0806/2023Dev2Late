b2420755112dca6d616865eea1db86a2019928e9
2101171 <2101171@s.asojuku.ac.jp>
2023-10-30 12:35:50 +0900

    コメントの整理

--- 04_ソースコード/public/backend/DBmng.php
+++ 04_ソースコード/public/backend/DBmng.php
@@ -6,9 +6,10 @@
             return $pdo;
         }
 
-//      $pdo = $this->dbConnect();
-
-        public function getUserScore($id){
+        // $pdo = $this->dbConnect();
+        
+        // スコア取得
+        public function getUserScoreById($id){
             $pdo = $this->dbConnect();
             $sql = "SELECT user_id, user_score FROM user WHERE user_id = ?";
             $ps = $pdo->prepare($sql);
@@ -18,31 +19,28 @@
             return $searchArray['user_score'];
         }
 
+        // スコア更新
         public function updateScore($new_score){
             session_start();
-            //try{
-                if(isset($_SESSION['user_id'])){
-                    require_once 'GetUserScore.php';
-                    $gus = new GetUserScore();
-                    $now_score = $gus->getUserScore($_SESSION['user_id']);
-                    if($new_score > $now_score){
-                        $pdo = $this->dbConnect();
-                        $sql = "UPDATE user SET user_score = ? WHERE user_id = ?";
-                        $ps = $pdo->prepare($sql);
-                        $ps->bindValue(1,$new_score,PDO::PARAM_INT);
-                        $ps->bindValue(2,$_SESSION['user_id'],PDO::PARAM_STR);
-                        $ps->execute();
-                    }
-                //}else{
-                //    throw new Exception("セッションエラー");
-                //}
-            //}catch(Exception $e){
-            //    echo $e->getMessage();
+            if(isset($_SESSION['user_id'])){
+                require_once 'GetUserScore.php';
+                $gus = new GetUserScore();
+                $now_score = $gus->getUserScore($_SESSION['user_id']);
+                if($new_score > $now_score){
+                    $pdo = $this->dbConnect();
+                    $sql = "UPDATE user SET user_score = ? WHERE user_id = ?";
+                    $ps = $pdo->prepare($sql);
+                    $ps->bindValue(1,$new_score,PDO::PARAM_INT);
+                    $ps->bindValue(2,$_SESSION['user_id'],PDO::PARAM_STR);
+                    $ps->execute();
+                }
             }
         }
 
+        // 新規ユーザー登録
         public function userCreation($id, $pass){
             $pdo = $this->dbConnect();
+            // 既存のIDと重複していないかチェックする
             $sql1 = "SELECT user_id FROM user";
             $ps = $pdo->prepare($sql1);
             $ps->execute();
@@ -52,6 +50,7 @@
                     return false;
                 }
             }
+            // 重複していなければ登録する処理を行う
             $sql2 = "INSERT user (user_id, user_password, user_score) VALUES (?, ?, 0)";
             $ps = $pdo->prepare($sql2);
             $ps->biadValue(1,$id,PDO::PARAM_STR);
