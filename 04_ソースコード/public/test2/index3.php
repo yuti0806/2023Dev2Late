7c5771de966d92a25b35057ab714847b20b6d253
masatokg <masatokg@users.noreply.github.com>
2023-11-01 14:49:12 +0900

    Create index3.php

---
+++ 04_ソースコード/public/test2/index3.php
@@ -0,0 +1,26 @@
+<?php
+echo "hello world <br>";
+// DBmng.phpを読み込む
+// require_once(__DIR__ . '/DBmng.php');
+echo "read php <br>";
+require_once $_SERVER['DOCUMENT_ROOT'] . '/backend//DBmng.php';
+// DBManagerクラスのインスタンスを生成する
+echo "new DBmng <br>";
+$db = new DBmng();
+// DBManagerクラスのgetDB関数を実行する
+echo "access DBmng <br>";
+$pdo = $db->dbConnect();
+echo "access DBmng done <br>";
+// データベースに接続できなかった場合
+if ($pdo == null) {
+    // エラーメッセージを表示する
+    echo 'データベースに接続できませんでした。';
+    // 処理を終了する
+    die();
+}
+// データベースに接続できた場合
+else {
+    // データベースに接続できたことを表示する
+    echo 'データベースに接続できました。';
+}
+echo "goodbye world";
