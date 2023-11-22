2ce2990006dc3a64a8308dbf98677ee494bb181b
masatokg <masatokg@users.noreply.github.com>
2023-11-22 16:10:53 +0900

    Update api.php

--- 04_ソースコード/public/testKuga/api.php
+++ 04_ソースコード/public/testKuga/api.php
@@ -1,3 +1,4 @@
+<html>
 <?php
 // require_once(__DIR__ . '/Setting.php');
 $json = filter_input(INPUT_POST, "json");
@@ -37,6 +38,7 @@ foreach ($json as $record) {
     $ps->bindValue(6, $answer, PDO::PARAM_INT);
     $ps->execute();
 }
+?>
 
 
 /*
@@ -44,35 +46,37 @@ class DBmng
 {
 
 
-    // private $DBhost = Setting::$DBhost;
-    // private $DBname = Setting::$DBname;
-    // private $DBuser = Setting::$DBuser;
-    // private $DBpass = Setting::$DBpass;
+// private $DBhost = Setting::$DBhost;
+// private $DBname = Setting::$DBname;
+// private $DBuser = Setting::$DBuser;
+// private $DBpass = Setting::$DBpass;
 
-    // データベースに接続してPDOインスタンスを生成する関数
-    public function dbConnect()
-    {
-        // require_once 'Setting.php';
-        // テスト用 
-        $pdo = new PDO('mysql:host=mysql219.phy.lolipop.lan;dbname=LAA1563424-tetrisdb;charset=utf8', 'LAA1563424', 'A2gxAdmYNxUCDe7');
-        return $pdo;
-    }
+// データベースに接続してPDOインスタンスを生成する関数
+public function dbConnect()
+{
+// require_once 'Setting.php';
+// テスト用
+$pdo = new PDO('mysql:host=mysql219.phy.lolipop.lan;dbname=LAA1563424-tetrisdb;charset=utf8', 'LAA1563424', 'A2gxAdmYNxUCDe7');
+return $pdo;
+}
 
-    // 新規レコード登録
-    public function insertQuiz($pdo, $question, $answers1, $answers2, $answers3, $answers4, $answer)
-    {
-        // $pdo = $this->dbConnect();
-        // 登録する処理
-        $sql2 = "INSERT quiz (id, question, answers1, answers1, answers1, answers1, answer) VALUES (null, ?, ?, ?, ?, ?, ?)";
-        $ps = $pdo->prepare($sql2);
-        $ps->bindValue(1, $question, PDO::PARAM_STR);
-        $ps->bindValue(2, $answers1, PDO::PARAM_STR);
-        $ps->bindValue(3, $answers2, PDO::PARAM_STR);
-        $ps->bindValue(4, $answers3, PDO::PARAM_STR);
-        $ps->bindValue(5, $answers4, PDO::PARAM_STR);
-        $ps->bindValue(6, $answer, PDO::PARAM_INT);
-        $ps->execute();
-        return true;
-    }
+// 新規レコード登録
+public function insertQuiz($pdo, $question, $answers1, $answers2, $answers3, $answers4, $answer)
+{
+// $pdo = $this->dbConnect();
+// 登録する処理
+$sql2 = "INSERT quiz (id, question, answers1, answers1, answers1, answers1, answer) VALUES (null, ?, ?, ?, ?, ?, ?)";
+$ps = $pdo->prepare($sql2);
+$ps->bindValue(1, $question, PDO::PARAM_STR);
+$ps->bindValue(2, $answers1, PDO::PARAM_STR);
+$ps->bindValue(3, $answers2, PDO::PARAM_STR);
+$ps->bindValue(4, $answers3, PDO::PARAM_STR);
+$ps->bindValue(5, $answers4, PDO::PARAM_STR);
+$ps->bindValue(6, $answer, PDO::PARAM_INT);
+$ps->execute();
+return true;
+}
 }
 */
+
+</html>
