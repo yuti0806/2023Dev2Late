ddb79431b1cf4f63c0d6aa08101d1fcd146c41d3
masatokg <masatokg@users.noreply.github.com>
2023-11-27 13:50:17 +0900

    Update tetris.js

--- 04_ソースコード/public/testTetrisJsKuga/tetris.js
+++ 04_ソースコード/public/testTetrisJsKuga/tetris.js
@@ -175,6 +175,56 @@ class Game {
             this.drawAll()
         }.bind(this)
     }
+
+    // タッチイベントリスナーをセット
+    setTouchEvent() {
+        // タップ時の誤動作を防ぐためのスワイプ時の処理を実行しない最小距離
+        const minimumDistance = 30
+        // スワイプ開始時の座標
+        let startX = 0
+        let startY = 0
+        // スワイプ終了時の座標
+        let endX = 0
+        let endY = 0
+
+        // 解説①：移動を開始した座標を取得
+        window.addEventListener('touchstart', (e) => {
+            startX = e.touches[0].pageX
+            startY = e.touches[0].pageY
+            alert();
+        })
+
+        // 解説②：移動した座標を取得
+        window.addEventListener('touchmove', (e) => {
+            endX = e.changedTouches[0].pageX
+            endY = e.changedTouches[0].pageY
+        })
+
+
+        // 解説③：移動距離から左右or上下の処理を実行
+        window.addEventListener('touchend', (e) => {
+            // スワイプ終了時にx軸とy軸の移動量を取得
+            // 左スワイプに対応するためMath.abs()で+に変換
+            const distanceX = Math.abs(endX - startX)
+            const distanceY = Math.abs(endX - startY)
+
+            // 左右のスワイプ距離の方が上下より長い && 小さなスワイプは検知しないようにする
+            if (distanceX > distanceY && distanceX > minimumDistance) {
+                // スワイプ後の動作
+                console.log('左右スワイプ')
+                alert('左右スワイプ');
+            }
+
+            // 上下のスワイプ距離の方が左右より長い && 小さなスワイプは検知しないようにする
+            if (distanceX < distanceY && distanceY > minimumDistance) {
+                // スワイプ後の動作
+                console.log('上下スワイプ')
+                alert('上下スワイプ');
+            }
+        })
+    }
+
+
 }
 
 class Block {
@@ -358,45 +408,45 @@ class Field {
     }
 }
 
-// タップ時の誤動作を防ぐためのスワイプ時の処理を実行しない最小距離
-const minimumDistance = 30
-// スワイプ開始時の座標
-let startX = 0
-let startY = 0
-// スワイプ終了時の座標
-let endX = 0
-let endY = 0
+// // タップ時の誤動作を防ぐためのスワイプ時の処理を実行しない最小距離
+// const minimumDistance = 30
+// // スワイプ開始時の座標
+// let startX = 0
+// let startY = 0
+// // スワイプ終了時の座標
+// let endX = 0
+// let endY = 0
 
-// 解説①：移動を開始した座標を取得
-window.addEventListener('touchstart', (e) => {
-    startX = e.touches[0].pageX
-    startY = e.touches[0].pageY
-    alert()
-})
+// // 解説①：移動を開始した座標を取得
+// window.addEventListener('touchstart', (e) => {
+//     startX = e.touches[0].pageX
+//     startY = e.touches[0].pageY
+//     alert();
+// })
 
-// 解説②：移動した座標を取得
-window.addEventListener('touchmove', (e) => {
-    endX = e.changedTouches[0].pageX
-    endY = e.changedTouches[0].pageY
-})
+// // 解説②：移動した座標を取得
+// window.addEventListener('touchmove', (e) => {
+//     endX = e.changedTouches[0].pageX
+//     endY = e.changedTouches[0].pageY
+// })
 
 
-// 解説③：移動距離から左右or上下の処理を実行
-window.addEventListener('touchend', (e) => {
-    // スワイプ終了時にx軸とy軸の移動量を取得
-    // 左スワイプに対応するためMath.abs()で+に変換
-    const distanceX = Math.abs(endX - startX)
-    const distanceY = Math.abs(endX - startY)
+// // 解説③：移動距離から左右or上下の処理を実行
+// window.addEventListener('touchend', (e) => {
+//     // スワイプ終了時にx軸とy軸の移動量を取得
+//     // 左スワイプに対応するためMath.abs()で+に変換
+//     const distanceX = Math.abs(endX - startX)
+//     const distanceY = Math.abs(endX - startY)
 
-    // 左右のスワイプ距離の方が上下より長い && 小さなスワイプは検知しないようにする
-    if (distanceX > distanceY && distanceX > minimumDistance) {
-        // スワイプ後の動作
-        console.log('左右スワイプ')
-    }
+//     // 左右のスワイプ距離の方が上下より長い && 小さなスワイプは検知しないようにする
+//     if (distanceX > distanceY && distanceX > minimumDistance) {
+//         // スワイプ後の動作
+//         console.log('左右スワイプ')
+//     }
 
-    // 上下のスワイプ距離の方が左右より長い && 小さなスワイプは検知しないようにする
-    if (distanceX < distanceY && distanceY > minimumDistance) {
-        // スワイプ後の動作
-        console.log('上下スワイプ')
-    }
-})
+//     // 上下のスワイプ距離の方が左右より長い && 小さなスワイプは検知しないようにする
+//     if (distanceX < distanceY && distanceY > minimumDistance) {
+//         // スワイプ後の動作
+//         console.log('上下スワイプ')
+//     }
+// })
