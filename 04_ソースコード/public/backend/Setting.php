718aaeca8e8a654deaef8c073443f1f8f76fa6ac
masatokg <masatokg@users.noreply.github.com>
2023-11-27 16:22:59 +0900

    delete chache

--- 04_ソースコード/public/backend/Setting.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Setting
-{
-    // 定数を切る
-    public static $DBhost = "mysql219.phy.lolipop.lan";
-    public static  $DBname = "LAA1563424-tetrisdb";
-    public static  $DBuser = "LAA1563424";
-    public static  $DBpass = "A2gxAdmYNxUCDe7";
-}
