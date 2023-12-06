6c275199e68ce7ea1e299de2c810b09fca2ac471
masatokg <masatokg@users.noreply.github.com>
2023-12-06 11:43:25 +0900

    Update Login.php

--- 04_ソースコード/public/backend/Login.php
+++ 04_ソースコード/public/backend/Login.php
@@ -4,27 +4,27 @@ require_once 'DBmng.php';
 $cls = new DBmng();
 if (empty($_POST['user_id'])) {
   $_SESSION['loginError'] = "user idを入力してください";
-  header('Location:../html/login.html');
+  header('Location:../test2/login.html');
   exit;
 } else if (empty($_POST['user_password'])) {
   $_SESSION['loginError'] = "Passwordを入力してください";
-  header('Location:../html/login.html');
+  header('Location:../test2/login.html');
   exit;
 }
 $searchArray = $cls->getUserTblByIdPass($_POST['user_id'], $_POST['user_password'], null);
 if (empty($searchArray)) {
   $_SESSION['loginError'] = "user idが存在しません";
-  header('Location:../html/login.html');
+  header('Location:../test2/login.html');
   exit;
 }
 foreach ($searchArray as $row) {
   if ($_POST['user_password'] == $row['user_password']) {
     $_SESSION['user_id'] = $row['user_id'];
-    header('Location:../html/index.html');
+    header('Location:../test2/index.html');
     exit;
   } else {
     $_SESSION['loginError'] = "パスワードが正しくありません";
-    header('Location:../html/login.html');
+    header('Location:../test2/login.html');
     exit;
   }
 }
