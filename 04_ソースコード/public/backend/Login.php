7764356ce75a0aeb140f34d2ea47b7bbe5897dc4
masatokg <masatokg@users.noreply.github.com>
2023-12-06 11:42:12 +0900

    Update Login.php

--- 04_ソースコード/public/backend/Login.php
+++ 04_ソースコード/public/backend/Login.php
@@ -1,31 +1,30 @@
-<?php 
+<?php
 session_start();
 require_once 'DBmng.php';
 $cls = new DBmng();
-if(empty($_POST['user_id'])){
+if (empty($_POST['user_id'])) {
   $_SESSION['loginError'] = "user idを入力してください";
   header('Location:../html/login.html');
   exit;
-}else if(empty($_POST['user_password'])){
+} else if (empty($_POST['user_password'])) {
   $_SESSION['loginError'] = "Passwordを入力してください";
   header('Location:../html/login.html');
   exit;
 }
-$searchArray = $cls->getUserTblByIdPass($_POST['user_id'],$_POST['user_password'],);
-if(empty($searchArray)){
-    $_SESSION['loginError'] = "user idが存在しません";
-      header('Location:../html/login.html');
-      exit;
+$searchArray = $cls->getUserTblByIdPass($_POST['user_id'], $_POST['user_password'], null);
+if (empty($searchArray)) {
+  $_SESSION['loginError'] = "user idが存在しません";
+  header('Location:../html/login.html');
+  exit;
 }
-foreach($searchArray as $row){
-    if($_POST['user_password'] == $row['user_password']){
-        $_SESSION['user_id'] = $row['user_id'];
-        header('Location:../html/index.html');
-      exit;
-    } else {
-      $_SESSION['loginError'] = "パスワードが正しくありません";
-      header('Location:../html/login.html');
-      exit;
-    }
+foreach ($searchArray as $row) {
+  if ($_POST['user_password'] == $row['user_password']) {
+    $_SESSION['user_id'] = $row['user_id'];
+    header('Location:../html/index.html');
+    exit;
+  } else {
+    $_SESSION['loginError'] = "パスワードが正しくありません";
+    header('Location:../html/login.html');
+    exit;
+  }
 }
-?>
