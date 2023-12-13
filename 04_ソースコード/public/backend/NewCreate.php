9676f124484beecab3cb929029cef5d1e4a38666
odanihayato <114891191+odanihayato@users.noreply.github.com>
2023-12-13 12:30:56 +0900

    a

--- 04_ソースコード/public/backend/NewCreate.php
+++ 04_ソースコード/public/backend/NewCreate.php
@@ -3,11 +3,11 @@ session_start();
 
 if (empty($_POST['user_id'])) {
       $_SESSION['createError'] = "user idを入力してください";
-      header('Location:../test2/newcreate.html');
+      header('Location:../test/newcreate.html');
       exit;
 } else if (empty($_POST['user_password'])) {
       $_SESSION['createError'] = "Passwordを入力してください";
-      header('Location:../test2/newcreate.html');
+      header('Location:../test/newcreate.html');
       exit;
 }
 
@@ -16,6 +16,6 @@ $cls = new DBmng();
 if ($_POST['create']) {
       $cls->userCreation($_POST['user_id'], $_POST['user_password'], null);
       $_SESSION['user_id'] = $_POST['user_id'];
-      header('Location:../test2/index.html');
+      header('Location:../test/index.html');
       exit;
 }
