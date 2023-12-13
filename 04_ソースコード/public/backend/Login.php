4e04ebf23afbf2920327583a78452ccbc46b33f1
odanihayato <114891191+odanihayato@users.noreply.github.com>
2023-12-13 12:26:07 +0900

    a

--- 04_ソースコード/public/backend/Login.php
+++ 04_ソースコード/public/backend/Login.php
@@ -4,28 +4,28 @@ require_once 'DBmng.php';
 $cls = new DBmng();
 if (empty($_POST['user_id'])) {
   $_SESSION['loginError'] = "user idを入力してください";
-  header('Location:../test2/login.html');
+  header('Location:../test/login.html');
   exit;
 } else if (empty($_POST['user_password'])) {
   $_SESSION['loginError'] = "Passwordを入力してください";
-  header('Location:../test2/login.html');
+  header('Location:../test/login.html');
   exit;
 }
 $searchArray = $cls->getUserTblByIdPass($_POST['user_id'], $_POST['user_password'], null);
 if (empty($searchArray)) {
   $_SESSION['loginError'] = "user idが存在しません";
-  header('Location:../test2/login.html');
+  header('Location:../test/login.html');
   exit;
 }
 foreach ($searchArray as $row) {
   if ($_POST['user_password'] == $row['user_password']) {
     $_SESSION['user_id'] = $row['user_id'];
-    header('Location:../test2/index.html');
+    header('Location:../test/index.html');
     $cls->getUserScoreById($_POST['user_id'], $pdo);
     exit;
   } else {
     $_SESSION['loginError'] = "パスワードが正しくありません";
-    header('Location:../test2/login.html');
+    header('Location:../test/login.html');
     exit;
   }
 }
