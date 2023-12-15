1843605bebd4454b4b9d37944b8f7dfc8c95968d
odanihayato <114891191+odanihayato@users.noreply.github.com>
2023-12-15 13:26:20 +0900

    kesita

--- 04_ソースコード/public/test/tetris.js
+++ 04_ソースコード/public/test/tetris.js
@@ -434,7 +434,7 @@ class Field {
             if (c === COLS_COUNT) {
                 this.blocks = this.blocks.filter(block => block.y !== r)
                 this.blocks.filter(block => block.y < r).forEach(upper => upper.y++)
-                alert("123");//完成前に消す
+                
                 // 例：id="point" のテキストを変更する
                 let l_scoreDom = document.getElementById("user_score");
                 // 現在のスコアを取得
