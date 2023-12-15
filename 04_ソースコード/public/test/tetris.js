312b572c42fc89ef79d1117413c419ef22315f6c
2101171 <2101171@s.asojuku.ac.jp>
2023-12-15 12:30:18 +0900

    Update tetris.js

--- 04_ソースコード/public/test/tetris.js
+++ 04_ソースコード/public/test/tetris.js
@@ -486,3 +486,45 @@ class Field {
 //         console.log('上下スワイプ')
 //     }
 // })
+
+function updatePoint(user_id) {
+    // ここに処理を追加してください
+    // 例：id="point" のテキストを変更する
+    let scoreDom = document.getElementById("highScore");
+    // 現在のスコアを取得
+    let newScore = scoreDom.value;
+    // 更新後のスコア表示覧
+    let updatedScoreDom = document.getElementById("user_Score");
+
+    // FormDataオブジェクトを作成
+    var formData = new FormData();
+
+    // データを追加
+    formData.append("user_id", user_id);
+    formData.append("newscore", newScore);
+    // formData.append("key2", "value2");
+
+    // Fetch APIを使用してPOSTリクエストを送信
+    fetch('https://team4.nikita.jp/backend/updatePoint.php', {
+        method: 'POST',  // メソッドを指定
+        body: formData  // ボディにFormDataを設定
+    })
+        // レスポンスを画面にセット
+        .then(response => {
+            //PromiseResult:Response
+            let text = response.text();
+            // console.log(text);
+            text.then(response2 => {
+                //PromiseResult:戻り値
+                console.log(response2);
+                // JSON形式の文字列が戻り値なので、オブジェクトに変換
+                let obj = JSON.parse(response2);
+                updatedScoreDom.innerHTML = obj.nowScore;
+            });
+        }).catch(error => {
+            console.log(error);
+        });
+
+
+    // alert(newScore);
+}
